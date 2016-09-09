f547c2e303b5af934851bb8f786d76fee9ba971a
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-09-09 16:21:42 +0530

    Maine naya Game banaya or yeh mera pehla commit tha

---
+++ Assets/Scripts/cube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+	Rigidbody ball; 
+
+	Vector3 vec = new Vector3(0,1,1); 
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ball.angularDrag = 12; 
+		ball.AddForce(Vector3.forward * 10f);
+	}
+}
